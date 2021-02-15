9945c6741ce98a6ad496ead597bf5e8fb4bfce1e
BryanCruz <bryan.bialokur@gmail.com>
2021-02-14 21:51:45 -0300

    feat: add some letters and place them

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,17 +1,38 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+	public GameObject letra;
+	public GameObject centroDaTela;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		centroDaTela = GameObject.Find( "centroDaTela" );
+		InitLetras();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	void InitLetras()
+	{
+		int numLetras = 5;
+		for ( int i = 0; i < numLetras; i++ )
+		{
+			float x = centroDaTela.transform.position.x + ((i - numLetras / 2.0f) * 80);
+			float y = centroDaTela.transform.position.y;
+			float z = centroDaTela.transform.position.z;
+
+			Vector3 novaPosicao = new Vector3( x, y, z );
+
+			GameObject l = Instantiate( letra, novaPosicao, Quaternion.identity );
+			l.name = "letra" + (i + 1);
+			l.transform.SetParent( GameObject.Find( "Canvas" ).transform );
+		}
+	}
 }
