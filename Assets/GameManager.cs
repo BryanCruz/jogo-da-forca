6d3b2cfe7228ae636b6d9852f49d41094c6faacf
BryanCruz <bryan.bialokur@gmail.com>
2021-02-14 22:04:48 -0300

    feat: add main variables to the game

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	public GameObject letra;
-	public GameObject centroDaTela;
+	public GameObject letra;                         // prefab da letra no Game
+	public GameObject centroDaTela;                  // objeto que indica o centro da tela
+
+	private string palavraOculta = "";               // palavra oculta a ser descoberta
+	private int tamanhoPalavraOculta = 0;            // tamanho da palavra oculta
+	private char[] letrasOcultas;                    // letras da palavra oculta
+	private bool[] letrasDescobertas;                // indicador de quais letras foram descobertas
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		centroDaTela = GameObject.Find( "centroDaTela" );
+		InitGame();
 		InitLetras();
 	}
 
@@ -21,7 +27,7 @@ public class GameManager : MonoBehaviour
 
 	void InitLetras()
 	{
-		int numLetras = 5;
+		int numLetras = tamanhoPalavraOculta;
 		for ( int i = 0; i < numLetras; i++ )
 		{
 			float x = centroDaTela.transform.position.x + ((i - numLetras / 2.0f) * 80);
@@ -34,5 +40,15 @@ public class GameManager : MonoBehaviour
 			l.name = "letra" + (i + 1);
 			l.transform.SetParent( GameObject.Find( "Canvas" ).transform );
 		}
+	}
+
+	void InitGame()
+	{
+		palavraOculta = "Elefante";                            // definição da palavra a ser descoberta
+		tamanhoPalavraOculta = palavraOculta.Length;           // número de letras da palavra
+		palavraOculta = palavraOculta.ToUpper();               // transforma a palavra para maiúscula
+
+		letrasOcultas = palavraOculta.ToCharArray();           // instancia-se o array de char e copia a palavra no array de char
+		letrasDescobertas = new bool[tamanhoPalavraOculta];    // instancia-se o array de bool
 	}
 }
