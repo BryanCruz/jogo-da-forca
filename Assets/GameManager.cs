affdd2d5dce0bcf1d76c57e6f1666b9ea7418770
BryanCruz <bryan.bialokur@gmail.com>
2021-02-14 22:34:49 -0300

    feat: get user input and show matched letters

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 	public GameObject letra;                         // prefab da letra no Game
 	public GameObject centroDaTela;                  // objeto que indica o centro da tela
 
-	private string palavraOculta = "";               // palavra oculta a ser descoberta
+	private string palavraOculta = "";               // palavra oculta a ser descoberta 
 	private int tamanhoPalavraOculta = 0;            // tamanho da palavra oculta
 	private char[] letrasOcultas;                    // letras da palavra oculta
 	private bool[] letrasDescobertas;                // indicador de quais letras foram descobertas
@@ -23,6 +24,7 @@ public class GameManager : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		CheckTeclado();
 	}
 
 	void InitLetras()
@@ -50,5 +52,31 @@ public class GameManager : MonoBehaviour
 
 		letrasOcultas = palavraOculta.ToCharArray();           // instancia-se o array de char e copia a palavra no array de char
 		letrasDescobertas = new bool[tamanhoPalavraOculta];    // instancia-se o array de bool
+	}
+
+	// Checa se o usuário digitou as letras corretas da palavra
+	// e as exibe se o usuário acertar
+	void CheckTeclado()
+	{
+		if ( Input.anyKeyDown )
+		{
+			char letraTeclada = Input.inputString.ToCharArray()[0];
+			int letraTecladaComoInt = System.Convert.ToInt32( letraTeclada );
+
+			if ( letraTecladaComoInt >= 97 && letraTecladaComoInt <= 122 )
+			{
+				letraTeclada = char.ToUpper( letraTeclada );
+				for ( int i = 0; i < tamanhoPalavraOculta; i++ )
+				{
+					if ( !letrasDescobertas[i] && letrasOcultas[i] == letraTeclada )
+					{
+						letrasDescobertas[i] = true;
+
+						Text componenteDaLetraI = GameObject.Find( "letra" + (i + 1) ).GetComponent<Text>();
+						componenteDaLetraI.text = letraTeclada.ToString();
+					}
+				}
+			}
+		}
 	}
 }
